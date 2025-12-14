368a03888afeccc0eb0dcbf4e10fee1ecb172772
adolfo.rafael.martinez <adolfo.rafael.martinez@gmail.com>
2025-12-14 04:28:47 +0000

    absolute path

--- create_wallet_ui.js
+++ create_wallet_ui.js
@@ -1,4 +1,4 @@
-import { ethers } from './node_modules/ethers/dist/ethers.esm.js';
+import { ethers } from '/node_modules/ethers/dist/ethers.esm.js';
 
 document.getElementById('createWalletBtn').addEventListener('click', async () => {
     const address = document.getElementById('addressInput').value;
