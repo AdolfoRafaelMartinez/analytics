80bc38dfef518f015b7416011ff074e9c6f9ec02
adolfo.rafael.martinez <adolfo.rafael.martinez@gmail.com>
2025-12-18 04:16:06 +0000

    display public

--- public/js/create_hd_wallet_ui.js
+++ public/js/create_hd_wallet_ui.js
@@ -17,14 +17,19 @@ createWalletBtn.addEventListener('click', () => {
                     <p><strong>Path:</strong> ${key.path}</p>
                     <p><strong>Address:</strong> ${key.address}</p>
                     <p><strong>Private Key (WIF):</strong> ${key.privateKey}</p>
+                    <p><strong>Public Key (WIF):</strong> ${key.publicKey}</p>
                 </div>
             `;
         });
         walletInfoDiv.innerHTML = `
             <p><strong>mnemonic:</strong> ${wallet.mnemonic}</p>
+            <div style="text-align: left; font-size: 2em; margin: 0.5em 0;">&darr;</div>
             <p><strong>seed:</strong> (Base64) ${wallet.seed.toBase64()}</p>
+            <div style="text-align: left; font-size: 2em; margin: 0.5em 0;">+</div>
             <p><strong>network:</strong> ${(wallet.network.messagePrefix.toLowerCase().includes("bitcoin")) ? "bitcoin test" : "unknown"}</p>
+            <div style="text-align: left; font-size: 2em; margin: 0.5em 0;">&darr;</div>
             <p><strong>root:</strong> (Base64) ${wallet.root.chainCode.toBase64()}</p>
+            <div style="text-align: left; font-size: 2em; margin: 0.5em 0;">&darr;</div>
             ${childKeysHtml}
             <hr>
         `;
