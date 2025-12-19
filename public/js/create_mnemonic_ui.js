7110c60ce94973f6d9f05c27e889dc0781478f9d
adolfo rafael martinez <adolfo.rafael.martinez@gmail.com>
2025-12-19 00:50:08 -0500

    creating mnemonic works

--- public/js/create_mnemonic_ui.js
+++ public/js/create_mnemonic_ui.js
@@ -1,20 +1,20 @@
 import bip39 from "bip39";
 const create_mnemonic = document.getElementById('create_mnemonic_button');
-const mnemonic_info_div = document.getElementById('mnemonic_info_div');
+const mnemonic_info_element = document.getElementById('mnemonic_info_div');
 const spinner = document.getElementById('spinner');
 
 create_mnemonic.addEventListener('click', () => {
-    mnemonic_info_div.innerHTML = '';
+    mnemonic_info_element.innerHTML = '';
     spinner.style.display = 'block';
 
     try {
         const mnemonic = bip39.generateMnemonic();
-        mnemonic_info_div.innerHTML = `
+        mnemonic_info_element.innerHTML = `
             <p><strong>mnemonic:</strong> ${mnemonic}</p>
         `;
     } catch (error) {
         console.error('Error creating mnemonic:', error);
-        mnemonic_info_div.innerHTML = "Error creating mnemonic. Please try again.";
+        mnemonic_info_element.innerHTML = "Error creating mnemonic. Please try again.";
     } finally {
         spinner.style.display = 'none';
     }
